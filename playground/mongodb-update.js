da86410fb80a0134c83b58563c9fc3108e80d14b
JC <jeanchristophe.gatien@gmail.com>
2018-04-09 14:59:46 -0400

    completed update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,13 +9,34 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp')
 
-  db.collection('Todos').findOneAndUpdate({
+/*  db.collection('Todos').findOneAndUpdate({
     _id: new ObjectID('5ac7c5437e1f595eb7b590db')
-  },{
+  }, {
     $set: {
       completed: true
     }
-  }
-  )
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });*/
+
+  //Challenge, update name, increment age
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5ac2e7bffa78dbf09bafd41a')
+  }, {
+    $set: {
+      name: "JC"
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
   //client.close();
 });
+//https://docs.mongodb.com/manual/reference/operator/update/
