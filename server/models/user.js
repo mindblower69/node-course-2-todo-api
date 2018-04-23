9b23700d154e25c1be96e3ecd14daa3ab89e4e90
JC <jeanchristophe.gatien@gmail.com>
2018-04-23 17:21:25 -0400

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -103,7 +113,6 @@ UserSchema.pre('save', function (next) { //You need to mention the next variable
   } else {
     next();
   }
-
 });
 
 var User = mongoose.model('User', UserSchema);
